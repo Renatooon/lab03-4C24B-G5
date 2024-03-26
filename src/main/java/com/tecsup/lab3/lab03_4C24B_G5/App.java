1ff2cfa82a7f90af14999abec197d96beeff1ede
alumno <alumno@localhost>
2024-03-26 14:54:53 -0500

    First commit

---
+++ src/main/java/com/tecsup/lab3/lab03_4C24B_G5/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab3.lab03_4C24B_G5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
